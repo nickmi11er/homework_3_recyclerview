1cb653e61b938da34830090ead0c118a67f3d7a2
a.guzenko <a.guzenko@tinkoff.ru>
2018-03-06 20:21:07 +0300

    base for RecyclerView homework

---
+++ app/src/main/java/com/tinkoff/androidcourse/MainActivity.java
@@ -0,0 +1,32 @@
+package com.tinkoff.androidcourse;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * Реализовать добавление тестовых работников
+                 */
+            }
+        });
+
+
+        /**
+         * Реализовать адаптер, выбрать любой LayoutManager и прикрутить это всё к RecyclerView
+         *
+         * Тестовые данные для отображения генерятся WorkerGenerator
+         */
+    }
+}
